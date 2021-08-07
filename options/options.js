6ef69659c2038b25251e70bddc722b3e5b0ec6db
Crowdbotics <team@crowdbotics.com>
2021-08-07 15:02:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cool_limit_29610Identifier",
+  url: "https://cool_limit_29610Identifier.botics.co",
+  api: "https://cool_limit_29610Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
